2abd93d56b40f5e8c9d99a4ce79928660ee9dd8e
CodeNinja04 <hemendrasharma2019@gmail.com>
2022-06-14 14:16:46 +0530

    changes

---
+++ contracts/ERC4626.sol
@@ -0,0 +1,242 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity >=0.8.0;
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20Lib} from "./utils/ERC20Lib.sol";
+import "hardhat/console.sol";
+
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {FixedPointMathLib} from "./utils/FixedPointMathLib.sol";
+
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+
+// EIP-4626: Tokenized Vault Standard
+
+contract Vault is ERC20Lib , ReentrancyGuard  {
+    using SafeERC20 for ERC20Lib;
+    using SafeERC20 for ERC20;
+    using FixedPointMathLib for uint256;
+
+
+
+    event Deposit(
+        address indexed caller,
+        address indexed owner,
+        uint256 assets,
+        uint256 shares
+    );
+
+    event Withdraw(
+        address indexed caller,
+        address indexed receiver,
+        address indexed owner,
+        uint256 assets,
+        uint256 shares
+    );
+
+    // underlying token managed by the Vault
+    ERC20 public asset;
+
+    // initialize function used in factory to set underlying asset token
+    function initialize(ERC20 _asset) public {
+        asset = _asset;
+    }
+
+    // deposit underlying asset to the contract and mint same shares as assets
+    function deposit(uint256 assets, address receiver)
+        
+        public
+        virtual
+        
+        returns (uint256 shares)
+    {
+        // Check for rounding error since we round down in previewDeposit.
+        require((shares = previewDeposit(assets)) != 0, "ZERO_SHARES");
+
+        // Need to transfer before minting or ERC777s could reenter.
+        //IERC20(asset).approve(address(this),assets);
+        asset.safeTransferFrom(msg.sender, address(this), assets);
+
+        _mint(receiver, shares);
+
+        emit Deposit(msg.sender, receiver, assets, shares);
+
+        afterDeposit(assets, shares);
+    }
+
+    // mint shares
+    function mint(uint256 shares, address receiver)
+        public
+        virtual
+        returns (uint256 assets)
+    {
+        assets = previewMint(shares);
+         // No need to check for rounding error, previewMint rounds up.
+        console.log("previewMint(shares)",previewMint(shares));
+
+        // Need to transfer before minting or ERC777s could reenter.
+        IERC20(asset).approve(address(this),assets);
+        asset.safeTransferFrom(msg.sender, address(this), assets);
+
+        _mint(receiver, shares);
+
+        emit Deposit(msg.sender, receiver, assets, shares);
+
+        afterDeposit(assets, shares);
+    }
+
+    // Burns shares from owner and sends exactly assets of underlying tokens to receiver.
+    function withdraw(
+        uint256 assets,
+        address receiver,
+        address owner
+    ) public virtual returns (uint256 shares) {
+        shares = previewWithdraw(assets); // No need to check for rounding error, previewWithdraw rounds up.
+
+        console.log(previewWithdraw(assets));
+        // console.log(msg.sender);
+        // console.log(owner);
+        if (msg.sender != owner) {
+            uint256 allowed = _allowances[owner][msg.sender]; // Saves gas for limited approvals.
+            console.log(allowed);
+            if (allowed != type(uint256).max && allowed!=0)
+             _allowances[owner][msg.sender] = allowed - shares;
+        }
+
+        
+
+        beforeWithdraw(assets, shares);
+
+        
+
+        _burn(owner, shares);
+
+        emit Withdraw(msg.sender, receiver, owner, assets, shares);
+
+        asset.transfer(receiver, assets);
+    }
+
+    // Burns exactly shares from owner and sends assets of underlying tokens to receiver.
+    function redeem(
+        uint256 shares,
+        address receiver,
+        address owner
+    ) public virtual returns (uint256 assets) {
+        if (msg.sender != owner) {
+            uint256 allowed = _allowances[owner][msg.sender]; // Saves gas for limited approvals.
+
+            if (allowed != type(uint256).max && allowed!=0)
+                _allowances[owner][msg.sender] = allowed - shares;
+        }
+
+        // Check for rounding error since we round down in previewRedeem.
+        require((assets = previewRedeem(shares)) != 0, "ZERO_ASSETS");
+
+        beforeWithdraw(assets, shares);
+
+        _burn(owner, shares);
+
+        emit Withdraw(msg.sender, receiver, owner, assets, shares);
+
+        asset.transfer(receiver, assets);
+    }
+
+    // amount of shares that the Vault would exchange for the amount of assets provided
+    function convertToShares(uint256 assets)
+        public
+        view
+        virtual
+        returns (uint256)
+    {
+        uint256 supply = totalSupply; // Saves an extra SLOAD if totalSupply is non-zero.
+
+        return
+            supply == 0
+                ? assets
+                : assets.mulDivDown(supply, asset.totalSupply());
+    }
+
+    //amount of assets that the Vault would exchange for the amount of shares provided
+    function convertToAssets(uint256 shares)
+        public
+        view
+        virtual
+        returns (uint256)
+    {
+        uint256 supply = totalSupply; // Saves an extra SLOAD if totalSupply is non-zero.
+
+        return
+            supply == 0
+                ? shares
+                : shares.mulDivDown(asset.totalSupply(), supply);
+    }
+
+    // Allows an on-chain or off-chain user to simulate the effects of their deposit at the current block, given current on-chain conditions.
+    function previewDeposit(uint256 assets)
+        public
+        view
+        virtual
+        returns (uint256)
+    {
+        return convertToShares(assets);
+    }
+
+    // Allows an on-chain or off-chain user to simulate the effects of their mint at the current block, given current on-chain conditions.
+
+    function previewMint(uint256 shares) public view virtual returns (uint256) {
+        uint256 supply = totalSupply; // Saves an extra SLOAD if totalSupply is non-zero.
+
+        return
+            supply == 0 ? shares : shares.mulDivUp(asset.totalSupply(), supply);
+    }
+
+    // Allows an on-chain or off-chain user to simulate the effects of their withdrawal at the current block, given current on-chain conditions.
+    function previewWithdraw(uint256 assets)
+        public
+        view
+        virtual
+        returns (uint256)
+    {
+        uint256 supply = totalSupply; // Saves an extra SLOAD if totalSupply is non-zero.
+
+        return
+            supply == 0 ? assets : assets.mulDivUp(supply, asset.totalSupply());
+    }
+
+    // Allows an on-chain or off-chain user to simulate the effects of their redeemption at the current block, given current on-chain conditions.
+    function previewRedeem(uint256 shares)
+        public
+        view
+        virtual
+        returns (uint256)
+    {
+        return convertToAssets(shares);
+    }
+
+    function maxDeposit(address) public view virtual returns (uint256) {
+        return type(uint256).max;
+    }
+
+    function maxMint(address) public view virtual returns (uint256) {
+        return type(uint256).max;
+    }
+
+    function maxWithdraw(address owner) public view virtual returns (uint256) {
+        return convertToAssets(balanceOf(owner));
+    }
+
+    function maxRedeem(address owner) public view virtual returns (uint256) {
+        return balanceOf(owner);
+    }
+
+    //INTERNAL HOOKS LOGIC
+
+    function beforeWithdraw(uint256 assets, uint256 shares) internal virtual {}
+
+    function afterDeposit(uint256 assets, uint256 shares) internal virtual {}
+
+     receive() external payable {}
+
+    // function earn() public {  }
+}
---
+++ contracts/proxy.sol
@@ -0,0 +1,147 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity >=0.8.0;
+
+import "@openzeppelin/contracts/proxy/Clones.sol";
+
+import {Vault} from "./ERC4626.sol";
+import {ERC20Lib} from "./utils/ERC20Lib.sol";
+
+import "./utils/IERC4626.sol";
+import "hardhat/console.sol";
+
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
+
+import {FixedPointMathLib} from "./utils/FixedPointMathLib.sol";
+
+contract factory is ReentrancyGuard {
+    using SafeERC20 for ERC20Lib;
+    using SafeERC20 for ERC20;
+    using FixedPointMathLib for uint256;
+    using Clones for address;
+    using Counters for Counters.Counter;
+    using Address for address;
+    Counters.Counter private pid;
+
+    address public vaultImplementation;
+
+    constructor(address _vaultImplementation) {
+        vaultImplementation = _vaultImplementation;
+    }
+
+    mapping(uint256 => address) public getVault;
+
+    address[] public allVaults;
+
+    event Depositvault(
+        address indexed caller,
+        uint256 indexed _pid,
+        uint256 assets
+    );
+
+    // create clones (minimal proxy eip 1167)
+    // it deployes vaults using minimal proxy
+    function createVault(
+        ERC20 _asset,
+        string memory name,
+        string memory symbol
+    ) public returns (address clone) {
+        bytes32 salt = keccak256(
+            abi.encodePacked(_asset, name, symbol, msg.sender)
+        );
+
+        clone = Clones.cloneDeterministic(vaultImplementation, salt);
+
+        IERC4626(clone).init(msg.sender, name, symbol, 10000e18);
+        IERC4626(clone).initialize(_asset);
+
+        getVault[pid.current()] = clone;
+        pid.increment();
+        allVaults.push(clone);
+
+        return clone;
+    }
+
+    // used to deposit assets to vauls via factory using _pid
+    function depositVault(
+        uint256 _amount,
+        address receiver,
+        uint256 _pid
+    ) public payable {
+        require(_amount > 0, "amount is less than 0");
+
+        ERC20(IERC4626(getVault[_pid]).asset()).approve(receiver, _amount);
+        ERC20(IERC4626(getVault[_pid]).asset()).approve(
+            getVault[_pid],
+            _amount
+        );
+
+        ERC20(IERC4626(getVault[_pid]).asset()).approve(address(this), _amount);
+
+        ERC20(IERC4626(getVault[_pid]).asset()).transferFrom(
+            msg.sender,
+            address(this),
+            _amount
+        ); // transfer asset from user to proxy
+
+        IERC4626((address(getVault[_pid]))).deposit(_amount, receiver); // trnasfer of asset from proxy to vault
+
+        emit Depositvault(msg.sender, _pid, _amount);
+    }
+
+    function withdrawVault(
+        uint256 _amount,
+        address receiver,
+        address owner,
+        uint256 _pid
+    ) public  {
+    
+    
+    IERC4626((address(getVault[_pid]))).withdraw(_amount, receiver, owner);
+
+        
+    }
+
+    function mintVault(
+        uint256 shares,
+        address receiver,
+        uint256 _pid
+    ) public payable {
+        uint256 assets = IERC4626((address(getVault[_pid]))).previewMint(
+            shares
+        );
+
+        console.log(
+            "mint",
+            IERC4626((address(getVault[_pid]))).previewMint(shares)
+        );
+
+        ERC20(IERC4626(getVault[_pid]).asset()).approve(receiver, shares);
+        ERC20(IERC4626(getVault[_pid]).asset()).approve(getVault[_pid], assets);
+       
+        
+        // console.log(all);
+        ERC20(IERC4626(getVault[_pid]).asset()).transferFrom(
+            msg.sender,
+            address(this),
+            assets
+        );
+       
+
+        IERC4626((address(getVault[_pid]))).mint(shares, receiver);
+    }
+
+
+
+function redeemVault(  uint256 _amount,
+        address receiver,
+        address owner,uint256 _pid) public {
+
+             IERC4626((address(getVault[_pid]))).withdraw(_amount, receiver, owner);
+
+        }
+    receive() external payable {}
+}
